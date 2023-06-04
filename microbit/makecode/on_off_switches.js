56ca4b948310d108b3bc7d445136e954f369e025
Martin Woolley <martin@woolleynet.com>
2023-06-04 12:09:23 +0100

    Added manual on off control using microbit buttons

--- microbit/makecode/on_off_switches.js
+++ microbit/makecode/on_off_switches.js
@@ -8,6 +8,12 @@ bluetooth.onBluetoothDisconnected(function () {
     basic.pause(1000)
     basic.clearScreen()
 })
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
 bluetooth.startIOPinService()
 pins.digitalWritePin(DigitalPin.P0, 0)
 basic.showIcon(IconNames.SmallDiamond)
